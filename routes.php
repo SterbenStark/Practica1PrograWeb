29d8169757c0c1c63490888b4653340d35a0ad61
SterbenStark <80368138+SterbenStark@users.noreply.github.com>
2021-04-29 18:59:29 -0600

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
     
-    Route::get('/',function() { echo 'Hello, SterbenStark, Tarea 1!'; });
+    Route::get('/',function() { echo 'Hello, SterbenStark, Tarea 1 -> Milton Gómez Pérez!'; });
     
     Route::dispatch();
 ?>
